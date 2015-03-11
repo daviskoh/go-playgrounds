6e77dd9f343d10aa1c4f2a6ca8c668dab8556eca
daviskoh <koh.davis.0@gmail.com>
2015-03-10 20:31:17 -0400

    defers use a stack

--- defer.go
+++ defer.go
@@ -6,7 +6,10 @@ import (
 
 func main() {
 	// defer keyword defers execution of func until wrapper func returns
+	// defer's are pushed onto a stack
 	defer fmt.Println("called last")
+	defer fmt.Println("called 3rd")
+	defer fmt.Println("called 2nd")
 
 	fmt.Println("called 1st")
 }
