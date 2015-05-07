c63ca3be387c11fe00e1dc61d74639b0ef779e1a
Davis Koh <koh.davis.0@gmail.com>
2015-05-07 17:13:26 -0400

    remove model dec

--- rest-api-ex/burgers/index.go
+++ rest-api-ex/burgers/index.go
@@ -6,16 +6,10 @@ import (
 	"net/http"
 )
 
-// dummy data for response
-type Burger struct {
-	patty      string
-	condiments []string
-}
-
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	burgers := []Burger{
-		Burger{patty: "beef"},
-		Burger{patty: "veggie"},
+		Burger{Patty: "beef"},
+		Burger{Patty: "veggie"},
 	}
 
 	if err := json.NewEncoder(w).Encode(burgers); err != nil {
